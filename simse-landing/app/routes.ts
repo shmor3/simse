1d0b1f75be2ecd96708e91dc56ba471781735498
shmor3 <44666181+shmor3@users.noreply.github.com>
2026-03-05 15:18:18 -0500

    feat(simse-landing): configure route mapping

---
+++ simse-landing/app/routes.ts
@@ -0,0 +1,6 @@
+import { type RouteConfig, index, route } from '@react-router/dev/routes';
+
+export default [
+	index('routes/home.tsx'),
+	route('unsubscribe', 'routes/unsubscribe.tsx'),
+] satisfies RouteConfig;
